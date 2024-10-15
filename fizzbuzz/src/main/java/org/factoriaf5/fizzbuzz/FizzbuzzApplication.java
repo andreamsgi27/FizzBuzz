09dfd2f7d2f001a0481e585d1b43c53e46183cf7
andreamsgi27 <andreamsgi27@hotmail.com>
2024-10-16 00:03:48 +0200

    Implemented FizzBuzz logic in main method.

--- fizzbuzz/src/main/java/org/factoriaf5/fizzbuzz/FizzbuzzApplication.java
+++ fizzbuzz/src/main/java/org/factoriaf5/fizzbuzz/FizzbuzzApplication.java
@@ -1,5 +1,7 @@
 package org.factoriaf5.fizzbuzz;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,25 @@ public class FizzbuzzApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FizzbuzzApplication.class, args);
+		Scanner scanner = new Scanner(System.in);
+
+		
+		System.out.print("Escriba un número: ");
+		int num =  scanner.nextInt();
+		
+		if (num % 3 == 0 && num % 5 == 0) {
+			System.out.println("FizzBuzz");
+		} else if (String.valueOf(num).contains("3") && String.valueOf(num).contains("5")) {
+			System.out.println("FizzBuzz");
+		} else if (num % 3 == 0 || String.valueOf(num).contains("3")) {
+			System.out.print("Fizz");
+		} else if (num % 5 == 0 || String.valueOf(num).contains("5")) {
+			System.out.print("Buzz");
+		} else {
+			System.out.println(num);
+		}
+
+		scanner.close();
 	}
 
 }
