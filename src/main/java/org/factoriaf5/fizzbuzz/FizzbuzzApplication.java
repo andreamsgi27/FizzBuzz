a5d61f6ed8f36fad1ea01fa24976d6d57a7e97d6
andreamsgi27 <andreamsgi27@hotmail.com>
2024-11-08 17:39:35 +0100

    Test cov 77%

--- src/main/java/org/factoriaf5/fizzbuzz/FizzbuzzApplication.java
+++ src/main/java/org/factoriaf5/fizzbuzz/FizzbuzzApplication.java
@@ -9,26 +9,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FizzbuzzApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FizzbuzzApplication.class, args);
-		Scanner scanner = new Scanner(System.in);
+        SpringApplication.run(FizzbuzzApplication.class, args);
+        Scanner scanner = new Scanner(System.in);
 
-		
-		System.out.print("Escriba un número: ");
-		int num =  scanner.nextInt();
-		
-		if (num % 3 == 0 && num % 5 == 0) {
-			System.out.println("FizzBuzz");
-		} else if (String.valueOf(num).contains("3") && String.valueOf(num).contains("5")) {
-			System.out.println("FizzBuzz");
-		} else if (num % 3 == 0 || String.valueOf(num).contains("3")) {
-			System.out.print("Fizz");
-		} else if (num % 5 == 0 || String.valueOf(num).contains("5")) {
-			System.out.print("Buzz");
-		} else {
-			System.out.println(num);
-		}
+        System.out.print("Escriba un número: ");
+        int num = scanner.nextInt();
 
-		scanner.close();
-	}
+        // Llamada a la lógica de FizzBuzz y salida del resultado
+        System.out.println(fizzBuzz(num));
+
+        scanner.close();
+    }
+
+    // Método que contiene la lógica del juego FizzBuzz
+    public static String fizzBuzz(int num) {
+        if (num % 3 == 0 && num % 5 == 0) {
+            return "FizzBuzz";
+        } else if (String.valueOf(num).contains("3") && String.valueOf(num).contains("5")) {
+            return "FizzBuzz";
+        } else if (num % 3 == 0 || String.valueOf(num).contains("3")) {
+            return "Fizz";
+        } else if (num % 5 == 0 || String.valueOf(num).contains("5")) {
+            return "Buzz";
+        } else {
+            return String.valueOf(num);
+        }
+    }
 
 }
--- src/test/java/org/factoriaf5/fizzbuzz/FizzbuzzApplicationTests.java
+++ src/test/java/org/factoriaf5/fizzbuzz/FizzbuzzApplicationTests.java
@@ -1,5 +1,6 @@
 package org.factoriaf5.fizzbuzz;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +10,78 @@ class FizzbuzzApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	public void testFizzBuzz() {
+		int a = 15;
+
+		String result = FizzbuzzApplication.fizzBuzz(a);
+		
+		assertEquals(result, "FizzBuzz");
+	}
+
+	@Test
+	public void testFizzBuzzCero() {
+		int a = 0;
+
+		String result = FizzbuzzApplication.fizzBuzz(a);
+		
+		assertEquals(result, "FizzBuzz");
+	}
+
+	@Test
+	public void testFizzBuzzNegative() {
+		int a = -1;
+
+		String result = FizzbuzzApplication.fizzBuzz(a);
+		
+		assertEquals(result, "-1");
+
+	}
+
+	@Test
+	public void testFizz() {
+		int a = 3;
+
+		String result = FizzbuzzApplication.fizzBuzz(a);
+		
+		assertEquals(result, "Fizz");
+	}
+
+	@Test
+	public void testBuzz() {
+		int a = 25;
+
+		String result = FizzbuzzApplication.fizzBuzz(a);
+		
+		assertEquals(result, "Buzz");
+	}
+
+	@Test
+	public void testFizzBuzzContains35() {
+		int a = 35;
+
+		String result = FizzbuzzApplication.fizzBuzz(a);
+		
+		assertEquals(result, "FizzBuzz");
+	}
+
+	@Test
+	public void testFizzBuzzContains3() {
+		int a = 13;
+
+		String result = FizzbuzzApplication.fizzBuzz(a);
+		
+		assertEquals(result, "Fizz");
+	}
+	@Test
+	public void testFizzBuzzContains5() {
+		int a = 52;
+
+		String result = FizzbuzzApplication.fizzBuzz(a);
+		
+		assertEquals(result, "Buzz");
+	}
+
 
 }
